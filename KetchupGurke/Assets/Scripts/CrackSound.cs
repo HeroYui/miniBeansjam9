ec24d6f520901baf95d82eb0be6c1b754d5272cc
Maximilian Schulze <max@ugschulze.de>
2023-11-04 21:09:11 +0100

    Fix crack sound not playing in intro scene

--- KetchupGurke/Assets/Scripts/CrackSound.cs
+++ KetchupGurke/Assets/Scripts/CrackSound.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class CrackSound : MonoBehaviour
 {
-    private Animator animator;
+    public AudioSource crackSoundSource;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    void PlayCrackSound()
     {
-        animator = GetComponent<Animator>();
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if()
+        crackSoundSource.enabled = true;
     }
 }
