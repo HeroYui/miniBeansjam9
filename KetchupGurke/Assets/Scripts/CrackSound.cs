041ebaa9d30ced8e1ae701c32919f25aee7688bb
s391357 <alanna.gerschler@stud-mail.uni-wuerzburg.de>
2023-11-04 17:59:33 +0100

    intro fast fertig

---
+++ KetchupGurke/Assets/Scripts/CrackSound.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackSound : MonoBehaviour
+{
+    private Animator animator;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
