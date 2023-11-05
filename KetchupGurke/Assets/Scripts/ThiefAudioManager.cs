a29c7a9138a6cfcc7840ca52874534dc275680da
HeroYui <5207909+HeroYui@users.noreply.github.com>
2023-11-05 17:52:09 +0100

    Added Thief Audio Manager Script

---
+++ KetchupGurke/Assets/Scripts/ThiefAudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefAudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip ThiefFootstep;
+    public AudioClip ThiefRummage;
+    public AudioClip ThiefLoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void playThiefFootstepSound()
+    {
+        audioSource.clip = ThiefFootstep;
+        audioSource.Play();
+    }
+
+    public void playThiefRummageSound()
+    {
+        audioSource.clip = ThiefRummage;
+        audioSource.Play();
+    }
+
+    public void playThiefLootSound()
+    {
+        audioSource.clip = ThiefLoot;
+        audioSource.Play();
+    }
+}
