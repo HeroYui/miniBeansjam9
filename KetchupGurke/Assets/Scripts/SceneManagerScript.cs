b13084fe140489de7e629fb79fb7617703bb9294
Maximilian Schulze <max@ugschulze.de>
2023-11-05 21:08:42 +0100

    Automate scene transition

--- KetchupGurke/Assets/Scripts/SceneManagerScript.cs
+++ KetchupGurke/Assets/Scripts/SceneManagerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,46 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
-    public float delay = 10f;
-    private float delayTimer;
-
-    private Scene scene;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       scene = SceneManager.GetActiveScene();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (scene.name.Equals("Intro")){
-            GameWirdGestartet();
-        }
-        
-    }
-
-    private void GameWirdGestartet()
-    {
-        //delayTimer = delayTimer + Time.deltaTime;
-        //if (delayTimer > delay)
-        //{
-            SceneManager.LoadSceneAsync(2);
-           // delayTimer = 0;
-        //}
-
-        
-    }
+    public string sceneToLoad;
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     public void Quit()
     {
         Application.Quit();
     }
+
 }
 
--- KetchupGurke/Assets/Scripts/UIScript.cs
+++ KetchupGurke/Assets/Scripts/UIScript.cs
@@ -6,15 +6,9 @@ public class UIScript : MonoBehaviour
 {
     public GameObject objectToDisable;
 
-    // Start is called before the first frame update
     void Start()
     {
         objectToDisable.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
